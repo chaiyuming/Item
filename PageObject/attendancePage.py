851d5e60a033dee405745707bc25a6a279be3e53
chaiyuming <13766462451@139.com>
2020-09-15 17:45:19 +0800

    modify testcase

--- PageObject/attendancePage.py
+++ PageObject/attendancePage.py
@@ -15,14 +15,13 @@ class Attendance(BasePage):
 
     title_top = (By.XPATH, '//*[@id="app"]/section/div[1]/div[3]/section/div/div[1]/strong')
     manage_loc = (By.XPATH, '//*[@id="app"]/section/div[1]/div[2]/div[4]/span')
-    # 考勤方案
-    place_loc = (By.XPATH, '//*[@id="app"]/section/div[1]/div[3]/aside/ul/li[1]/ul/li[2]')
-    state_loc = (By.XPATH, '//*[@id="app"]/section/div[1]/div[3]/section/div/div[2]/section/div[1]/div[2]/div[1]/div/form/div[1]/div/div/div/input')
-    inforbidden_loc = (By.XPATH, '/html/body/div[2]/div[1]/div[1]/ul/li[1]')
-    query_loc = (By.XPATH, '//*[@id="app"]/section/div[1]/div[3]/section/div/div[2]/section/form/div[2]/div/button[1]')
-    check_state_value=(By.XPATH,'//*[@id="app"]/section/div[1]/div[3]/section/div/div[2]/section/div[4]/div[3]/table/tbody/tr/td[10]/div')
-
-    error_windows=(By.CLASS_NAME,'el-message-warning')
+    # 年假额度
+    holidaymanage=(By.XPATH,'//*[@id="app"]/section/div[1]/div[3]/aside/ul/li[2]/div')
+    quota_loc = (By.XPATH, '//*[@id="app"]/section/div[1]/div[3]/aside/ul/li[2]/ul/li[1]')
+    name_loc = (By.XPATH, '//*[@id="leaveQuery"]/div[1]/div/form/div[3]/div/div/input')
+    query_loc = (By.XPATH, '//*[@id="leaveQuery"]/div[2]/div/form/div/div/button[1]')
+    check_name_value=(By.XPATH,'//*[@id="leaveQuery"]/div[3]/div[3]/table/tbody/tr/td[3]/div')
+    result_name_title=(By.XPATH,'//*[@id="leaveQuery"]/div[3]/div[2]/table/thead/tr/th[3]/div')
     # 员工请假
     vacation_loc=(By.XPATH,'//*[@id="app"]/section/div[1]/div[3]/aside/ul/li[2]')
     vacation_check_enter=(By.XPATH,'//*[@id="app"]/section/div[1]/div[3]/aside/ul/li[2]/ul/li[2]')
@@ -33,32 +32,33 @@ class Attendance(BasePage):
 
     def place(self):
         '''
-        考勤地点查询状态字段
+        年假额度查询name字段
         :return:
         '''
         WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.manage_loc))
-        WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.place_loc))
-        WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.state_loc))
-        try:
-            WebDriverWait(self.driver, 10, 1).until(EC.visibility_of_element_located(self.inforbidden_loc))
-            self.logHandle.info('the element is visibility')
-            self.find_element(*self.inforbidden_loc).click()
-            self.logHandle.info('click success')
-        except:
-            self.logHandle.debug('try again')
-            WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.title_top))
-            self.logHandle.info('start click state button')
-            WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.state_loc))
-            WebDriverWait(self.driver, 20, 1).until(EC.visibility_of_element_located(self.inforbidden_loc))
-            self.find_element(*self.inforbidden_loc).click()
+        WebDriverWait(self.driver, 10, 1).until(EC.element_can_be_clickable(self.holidaymanage))
+        if WebDriverWait(self.driver, 20, 1).until(EC.visibility_of_element_located(self.quota_loc)):
+            self.find_element(*self.quota_loc).click()
+        else:
+            self.logHandle.error('it is invisibility !!')
+
+        WebDriverWait(self.driver, 20, 1).until(EC.visibility_of_element_located(self.name_loc))
+        self.logHandle.info('the element is visibility')
+        # self.find_element(*self.name_loc).click()
+        self.find_element(*self.name_loc).send_keys('傅开云')
+        self.logHandle.info('input name success')
+
         time.sleep(1)
         self.find_element(*self.query_loc).click()
-        if WebDriverWait(self.driver,20,1).until(EC.visibility_of_element_located(self.error_windows)):
-            self.logHandle.info('it is not change company orangnazation')
-        allcheckout=self.find_elements(*self.check_state_value)
+        if WebDriverWait(self.driver,20,1).until(EC.visibility_of_element_located(self.result_name_title)):
+            self.logHandle.info('checkout success')
+            allcheckout=self.find_elements(*self.check_name_value)
+        else:
+            self.logHandle.info('checkout failed !!')
+            allcheckout=[]
         result=0
         for checkout in allcheckout:
-            if checkout.text =='启用':
+            if checkout.text =='傅开云':
                 result=1
                 self.logHandle.info('result is %s'% result)
             # elif checkout.text =='':
